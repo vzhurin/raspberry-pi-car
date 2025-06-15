29f32f76fd3976f92ab4ed694776eea96d6770b4
basil <zhurin.vs@gmail.com>
2025-06-16 00:01:25 +0300

    control

--- static/control/control.js
+++ static/control/control.js
@@ -1,24 +1,55 @@
 $(document).ready(function () {
-    setInterval(function (){
-        lval = $('#control-left').val() / 100;
-        rval = $('#control-right').val() / 100;
+    speedCoefficient = 1
+    timeout = 100
 
-        if (lval === 0 && rval === 0) {
-            return
+    setInterval(function (){
+        bfVal = Number($('#control-back-forward').val());
+        lrVal = Number($('#control-left-right').val());
+
+        console.log({bf: bfVal, lr: lrVal})
+
+        lVal = rVal = (-bfVal / 100)
+
+        if (lrVal < 0) {
+            lVal = lVal + lrVal
+
+            if (Math.abs(lVal) > 1) {
+                if (lVal < 0) {
+                    lVal = -1
+                }
+
+                if (lVal > 1) {
+                    lVal = 1
+                }
+            }
         }
 
-        console.log({left: lval, right: rval})
+        if (lrVal > 0) {
+            rVal = rVal - lrVal
+
+            if (Math.abs(rVal) > 1) {
+                if (rVal < 0) {
+                    rVal = -1
+                }
+
+                if (rVal > 1) {
+                    rVal = 1
+                }
+            }
+        }
+
+        console.log({l: lVal, r: rVal})
 
         $.ajax("http://192.168.1.35:8070/api/move", {
-            data: JSON.stringify({"rightDutyCycle": rval, "leftDutyCycle": lval}),
+            data: JSON.stringify({"leftDutyCycle": lVal, "rightDutyCycle": rVal}),
             contentType: 'application/json',
             type: 'POST'
         })
 
-    }, 500)
+    }, timeout)
 
     $('#control-stop').on('click', function (){
-        $('#control-left').val(0);
-        $('#control-right').val(0);
+        $('#control-back-forward').val(0);
+        $('#control-left-right').val(0);
     })
 })
