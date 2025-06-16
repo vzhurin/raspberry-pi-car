3d43c9a6e4148f10f3ff46688b3dc8456828220e
basil <zhurin.vs@gmail.com>
2025-06-16 19:55:23 +0300

    control

--- static/control/control.js
+++ static/control/control.js
@@ -1,23 +1,13 @@
 $(document).ready(function () {
-    let speedCoefficient = 0.4
+    let speedCoefficient = 0.7
     let timeout = 100
 
-    let bfEl = $('#control-back-forward');
-    let lrEl = $('#control-left-right');
+    let lEl = $('#control-left');
+    let rEl = $('#control-right');
 
     setInterval(function (){
-        let bfVal = Number(bfEl.val());
-        let lrVal = Number(lrEl.val());
-
-        console.log({bf: bfVal, lr: lrVal})
-
-        let lVal= (-bfVal / 100)
-        let rVal = (-bfVal / 100)
-
-
-
-        lVal = lVal * speedCoefficient
-        rVal = rVal * speedCoefficient
+        let lVal = (Number(lEl.val()) / -100) * speedCoefficient;
+        let rVal = (Number(rEl.val()) / -100) * speedCoefficient;
 
         console.log({l: lVal, r: rVal})
 
@@ -29,11 +19,11 @@ $(document).ready(function () {
 
     }, timeout)
 
-    bfEl.on('change', function () {
-        bfEl.val(0)
+    lEl.on('change', function () {
+        lEl.val(0)
     });
 
-    lrEl.on('change', function () {
-        lrEl.val(0)
+    rEl.on('change', function () {
+        rEl.val(0)
     });
 })
