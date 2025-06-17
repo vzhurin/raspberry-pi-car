203dc5ec2adfc4ed9c1068400b75361db25f0b21
basil <zhurin.vs@gmail.com>
2025-06-17 19:52:16 +0300

    control

--- static/control/control.js
+++ static/control/control.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
+    let carHost = "192.168.1.35"
     let speedCoefficient = 0.7
     let timeout = 100
 
-    let lEl = $('#control-left');
-    let rEl = $('#control-right');
+    let lEl = $("#control-left");
+    let rEl = $("#control-right");
 
     setInterval(function (){
         let lVal = (Number(lEl.val()) / -100) * speedCoefficient;
@@ -11,19 +12,19 @@ $(document).ready(function () {
 
         console.log({l: lVal, r: rVal})
 
-        $.ajax("http://192.168.1.35:8070/api/move", {
+        $.ajax("http://"+carHost+":8070/api/move", {
             data: JSON.stringify({"leftDutyCycle": lVal, "rightDutyCycle": rVal}),
-            contentType: 'application/json',
-            type: 'POST'
+            contentType: "application/json",
+            type: "POST"
         })
 
     }, timeout)
 
-    lEl.on('change', function () {
+    lEl.on("change", function () {
         lEl.val(0)
     });
 
-    rEl.on('change', function () {
+    rEl.on("change", function () {
         rEl.val(0)
     });
 })
